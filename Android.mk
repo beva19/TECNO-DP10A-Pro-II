93a5303f70630641f7c41a40894c4e60562936c9
beva19 <46389275+beva19@users.noreply.github.com>
2022-08-25 18:45:26 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,10 @@
 # Replace ${_CODENAME_} with your Device CodeName's Value. Mine is Primo_RX5.
 # Replace ${_VENDORNAME_} with your Brand/Vendor/Manufacturer's Value, Mine is WALTON 
 
-ifneq ($(filter ${_tecno_dp10apro_},$(TARGET_DEVICE)),)
-LOCAL_PATH := device/${_TECNO_}/${_tecno_dp10apro_}
+ifneq ($(filter tecno_dp10apro,$(TARGET_DEVICE)),)
+
+LOCAL_PATH := device/TECNO/tecno_dp10apro
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
 endif
